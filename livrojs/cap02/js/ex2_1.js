b9bf34ffb6bde2011c027ea8e5407bb489eb068a
lucas <lucasdjgualberto@gmail.com>
2025-08-27 21:46:01 -0300

    finalizando a aula

--- livrojs/cap02/js/ex2_1.js
+++ livrojs/cap02/js/ex2_1.js
@@ -5,6 +5,6 @@ const resp = document.querySelector("h3");
 // cria um "ouvinte" de evento, acionando quando botão submit for clicado
 frm.addEventListener("submit", (e) => {
     const nome = frm.inNome.value; // obtém o nome digitado no form
-    resp.innerText = `Olá ${nome}`;//exibe a resposta do programa
+    resp.innerHTML = `Olá <i>${nome}<i>`;//exibe a resposta do programa
     e.preventDefault(); // evita envio do form
 });
