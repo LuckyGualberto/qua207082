d3f44978d8b35388e55b7b5383451b31da03d6cb
lucas <lucasdjgalberto@gmail.com>
2025-08-29 22:39:18 -0300

    corrigido

--- livrojs/cap02/js/mercado.js
+++ livrojs/cap02/js/mercado.js
@@ -9,6 +9,6 @@ frm.addEventListener('submit', (e)=>{
     const preco = frm.inPreco.value;
     const promocao = (preco * 2) + (preco * 0.5);
 
-    outPromocao.innerHTML = `${produto} - Promoção : Leve três por ${promocao.toFixed(2)}`;
+    outPromocao.innerHTML = `${produto} - Promoção : Leve três por ${promocao.toFixed(2)}<br>`;
     outPromocao.innerHTML += ` O terceiro produto sai por ${(preco * 0.5).toFixed(2)}`;
 })
