f49c3f1a070a3e06d333f5527c615daa8fdf2cae
lucas <lucasdjgualberto@gmail.com>
2025-09-05 19:34:09 -0300

    Resolvidos

--- livrojs/cap04/js/4.d.js
+++ livrojs/cap04/js/4.d.js
@@ -2,38 +2,46 @@
 //um dos lados nao pode ser maior que a soma dos dois
 //Equilátero (3 lados iguais), Isósceles (2 lados iguais) e Escaleno (3 lados diferentes)
 
-const frm = document.querySelector("frm")
-const resp1 = document.querySelector("Lados")
-const resp2 = document.querySelector("Tipo")
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#Lados")
+const resp2 = document.querySelector("#Tipo")
 
 frm.addEventListener("submit", (e) =>{
     e.preventDefault()
 
-    const lado1 = frm.Lado1.value
-    const lado2 = frm.Lado2.value
-    const lado3 = frm.Lado3.value
+    const lado1 = frm.LadoA.value
+    const lado2 = frm.LadoB.value
+    const lado3 = frm.LadoC.value
     
 
     if (lado1 > lado2 + lado3 || lado2 > lado1+lado3 || lado3> lado1+lado2){
         resp1.innerText = `Não pode formar o trângulo`
-    }else if(lado1 == lado){
-
-    }    
+    }else if(lado1 == lado2 && lado1 == lado3 && lado2 == lado3){
+        resp1.innerText = `Lados podem formar um triângulo`
+        resp2.innerText = `Tipo: Equilátero`
+    }else if(lado1 == lado2 && lado1 == lado3 && lado2 != lado3){
+         resp1.innerText = `Lados podem formar um triângulo`
+         resp2.innerText = `Tipo: Isósceles`
+    }else if(lado1 != lado2 && lado1 == lado3 && lado2 == lado3){
+         resp1.innerText = `Lados podem formar um triângulo`
+         resp2.innerText = `Tipo: Isósceles`
+    }else if(lado1 == lado2 && lado1 != lado3 && lado2 == lado3){
+         resp1.innerText = `Lados podem formar um triângulo`
+         resp2.innerText = `Tipo: Isósceles`
+    }else {
+        resp1.innerText = `Lados podem formar um triângulo`
+        resp2.innerText = `Tipo: Escaleno`
+    }
 
 
     
-
-
-
-
-
-
 })
 
 
 
-/*  if((lado1> lado2) || (lado2 > lado3) || (lado3 > lado1) || (lado1 > lado3) || (lado2>lado1) || (lado3>lado2)){
-        resp1.innerText = `Não pode formar o trângulo`
-    }else if(){
 
-    }*/
+
+
+
+
+
