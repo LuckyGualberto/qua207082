33152440a7e84a4a65f46889cc9fc29f5108fea1
lucas <lucasdjgualberto@gmail.com>
2025-09-10 21:43:59 -0300

    Exemplos de laços

--- livrojs/cap05/js/ex5_6.js
+++ livrojs/cap05/js/ex5_6.js
@@ -7,12 +7,13 @@ frm.addEventListener("submit", (e) => {
     let primo = true // flag
     // percorre os possíveis divisores impares iniciado do 3 até a metade do número
     for(let i = 3; i <= numero /2; i = i + 2){ // percorre todos os possíveis divisores nde numero
-        if (numero % i == 0 ){ // verifica se i (3,5,7 ...) é divisor do numero
+
+        if (numero % i == 0 || numero % 2 == 0){ // verifica se i (3,5,7 ...) é divisor do numero
             primo = false // mudar a flag
             break // sair do loop
         }
     } 
-    if((numero <= 3 || numero % 2 != 0)&& primo){
+    if(numero <= 3 || primo){
         resp.innerText = `${numero} é primo`
     }else{
         resp.innerText = `${numero} NÂO é primo`
