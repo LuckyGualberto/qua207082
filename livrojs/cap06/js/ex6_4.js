f3a6a408a8bf937f7cd1abd22218573b75f830b5
lucas <lucasdjgualberto@gmail.com>
2025-09-18 21:06:35 -0300

    ainda vetores

--- livrojs/cap06/js/ex6_4.js
+++ livrojs/cap06/js/ex6_4.js
@@ -18,8 +18,8 @@ frm.btListar.addEventListener("click",()=>{
     }
     // método reduce() concatena uma string, obtendo nome e idade de cada criança
     const lista = criancas.reduce((acumulador,crianca) =>
-        acumulador + crianca.nome + `-R$: ${crianca.idade}\n`,"")
-    resp.innerText = `Lista dos Carros Cadastrados\n${"-".repeat(40)}\n${lista}`
+        acumulador + crianca.nome + ` ${crianca.idade}\n`,"")
+    resp.innerText = `Lista das crianças \n${"-".repeat(40)}\n${lista}`
 })
 // dispara o evento de click btListar (Equivale a um click no botão)
 frm.btListar.dispatchEvent(new Event("click"))
@@ -35,4 +35,47 @@ frm.addEventListener("submit", (e) =>{
     frm.inNome.focus()
     //atualiza a lista dos criancas na tela
     frm.btListar.dispatchEvent(new Event("click"))
+})
+
+frm.btResumir.addEventListener("click", () => {
+    // verificar se a lista está zerada?
+    if(criancas == 0){
+        alert("Não há crianças na lista")
+        return
+    }
+    //criar a copia do vetor criança
+    const copia = [...criancas]
+    //ordenar por idade
+    copia.sort((a,b) => a.idade - b.idade)
+    //criar uma variável para concatenar o retorno
+    let resumo =""
+    //salvar a menor idade do vetor
+    let aux = copia[0].idade
+    //criar um vetor para gravar a lista dos nomes por idade
+    let nomes = []
+    //iniciar um loop para percorrer o vetor de cópia
+    for(const crianca of copia){
+        const{nome, idade} = crianca
+        //veridicar se a idade permanece igual 
+        if(aux == idade){
+            // se sim incluir o nome da criança na lista nomes
+            nomes.push(nome)
+        }else{
+        //senão montar resumo para cada idade
+        resumo +=`${aux} ano(s) : ${nomes.length} criança(s) - `
+        resumo += ((nomes.length/copia.length)*100).toFixed(2)+"%\n"
+        resumo += `(${nomes.join(", ")})\n\n`
+        // atualiza a idade aux
+        aux = idade
+        //limpa o vetor nome
+        nomes = []
+        //adiciona o primeiro nome da nova idade
+        nomes.push(nome)
+        }
+
+    }
+    resumo +=`${aux} ano(s) : ${nomes.length} criança(s) - `
+    resumo += ((nomes.length/copia.length)*100).toFixed(2)+"%\n"
+    resumo += `(${nomes.join(", ")})\n\n`
+    resp.innerText = resumo
 })
