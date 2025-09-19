1ce72e37def112825f6ed9a86528edac55b11e00
lucas <lucasdjgualberto@gmail.com>
2025-09-18 21:47:28 -0300

    ex

---
+++ livrojs/cap06/.js/ex6.a.js
@@ -0,0 +1,8 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+const times = []
+
+frm.addEventListener ("submit", (e)=>{
+    e.preventDefault()
+    const time = 
+})
