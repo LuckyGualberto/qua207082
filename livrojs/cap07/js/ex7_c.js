66d5d6f3429a0c07217b46f02afc81bad20effab
lucas <lucasdjgualberto@gmail.com>
2025-09-30 20:02:00 -0300

    aula 250930: Exemplo 1 cap 8_1

--- livrojs/cap07/js/ex7_c.js
+++ livrojs/cap07/js/ex7_c.js
@@ -3,17 +3,18 @@ const resp1 = document.querySelector("#outResp1")
 const resp2 = document.querySelector("#outResp2")
 
 frm.addEventListener("submit", (e)=>{
+    // teste
     e.preventDefault()
     const dataInfracao = frm.inDataInfracao.value.split("-")
     const valorMulta = Number(frm.inValorMulta.value)
     const dataDesconto = new Date()
     dataDesconto.setFullYear(Number(dataInfracao[0]))
-    dataDesconto.setMonth(Number(dataInfracao[1]+1))
+    dataDesconto.setMonth(Number(dataInfracao[1])-1)
     dataDesconto.setDate(Number(dataInfracao[2])+90)
     // dataDesconto = dataDesconto + new Date(1000*60*60*24*90)
-    resp1.innerText = "Data Limite para Pagamento com Desconto"
-    resp1.innerText += dataDesconto.getDate().toString.padStar(2,"0")+"/"
-    resp1.innerText += (dataDesconto.getMonth()+1).toString.padStar(2,"0")+"/"
+    resp1.innerText = "Data Limite para Pagamento com Desconto: "
+    resp1.innerText += dataDesconto.getDate().toString().padStart(2,"0")+"/"
+    resp1.innerText += (dataDesconto.getMonth()+1).toString().padStart(2,"0")+"/"
     resp1.innerText += dataDesconto.getFullYear()
     resp2.innerText = "Valor com desconto R$"+(valorMulta*0.8).toFixed(2)
 })    
