09180728efd491f67ee38c6f155a2364d07d5fba
lucas <lucasdjgualberto@gmail.com>
2025-10-02 21:45:12 -0300

    Ex

--- livrojs/cap08/js/ex8_a.js
+++ livrojs/cap08/js/ex8_a.js
@@ -1,14 +1,16 @@
 const frm = document.querySelector("form")
 const resp = document.querySelector("pre")
+const nome = frm.inNome.value
+const idade = frm.inIdade.value
 
 frm.addEventListener("submit", (e)=>{
     e.preventDefault()
-    const nome = frm.inNome.value
-    const idade = frm.inIdade.value
+    
     resp.innerText = `${nome}`
 })
 
 function retornarTracos(){
-    
+    tracos = ""
+    let i
 }
 
