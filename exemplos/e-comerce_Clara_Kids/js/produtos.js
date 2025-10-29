3cf3853dd8803d526abd36ab18b88d51853b6959
lucas <lucasdjgualberto@gmail.com>
2025-10-28 21:47:14 -0300

    e_commerce Klara kids

---
+++ exemplos/e-comerce_Clara_Kids/js/produtos.js
@@ -0,0 +1,68 @@
+const lsProdutos = [
+    {
+        nome: "Conjunto Bobbie Goods",
+        descricao: "conjunto com jaqueta, blusinha e shortinho",
+        img: "img/roupa1 (1).png",
+        valor: 79.99
+    },
+    {
+        nome: "Conjunto Laço",
+        descricao: "Saia e cropped",
+        img: "img/roupa1 (2).png",
+        valor: 94.99
+    },
+    {
+        nome: "Crocs",
+        descricao: "Calçado",
+        img: "img/roupa1 (3).png",
+        valor: 94.99
+    },
+    {
+        nome: "Conjunto 'Queridinho' ",
+        descricao: "Camiseta adidas e saia de couro",
+        img: "img/roupa1 (4).png",
+        valor: 99.99
+    },
+    {
+        nome: "Conjunto 'Los Angeles'",
+        descricao: "Tom sobre tom rosa",
+        img: "img/roupa1 (5).png",
+        valor: 99.99
+    },
+    {
+        nome: "Conjunto 'Los Angeles'",
+        descricao: "Tom sobre tom azul",
+        img: "img/roupa1 (6).png",
+        valor: 99.99
+    },
+    {
+        nome: "Conjunto inspiração farm",
+        descricao: "Cropped e saia com tons tropicais",
+        img: "img/roupa1 (7).png",
+        valor: 89.99
+    },
+    {
+        nome: "Lançamento: Top boho e short",
+        descricao: "Lançamento: Top boho e short",
+        img: "img/roupa1 (8).png",
+        valor: 94.99
+    },
+    {
+        nome: "Conjunto caribeño",
+        descricao: "Conjunto de cropped e saia",
+        img: "img/roupa1 (9).png",
+        valor: 94.99
+    },
+    {
+        nome: "Conjunto Tropicalia",
+        descricao: "Cropped laçado e saia",
+        img: "img/roupa1 (10).png",
+        valor: 89.99
+    },
+    {
+        nome: "Conjunto Merengue",
+        descricao: "Cropped e saia com a fivela acentuada",
+        img: "img/roupa1 (11).png",
+        valor: 89.99
+    }
+]
---
+++ exemplos/e-comerce_Clara_Kids/js/script.js
@@ -0,0 +1,103 @@
+const original = document.querySelector(".produto")
+const produtos = document.querySelector(".produtos")
+const divcarrinho = document.querySelector(".carrinho")
+
+produtos.innerHTML = ""
+for (const p of lsProdutos){
+const clone = original.cloneNode(true)
+clone.querySelector(".nome").innerText = p.nome
+clone.querySelector(".descricao").innerText = p.descricao
+clone.querySelector("img").src = p.img
+clone.querySelector(".valor").innerText = `R$ ${p.valor.toFixed(2).toString().replace(".",",")}`
+produtos.appendChild(clone)
+}
+
+document.querySelectorAll(".produto").forEach((p,i) =>{
+    p.addEventListener("click", ()=>{
+        p.classList.toggle("marcado")
+        if(lsProdutos[i].qt == undefined){
+            lsProdutos[i].qt = 1
+        }else{
+            delete lsProdutos[i].qt
+        }
+        atualizarQt()
+    })
+})
+
+function atualizarQt(){
+    const qt = lsProdutos.filter(p => p.qt >= 1)
+    document.querySelector("#qt").innerText = qt.length
+    if(qt.length == 0){
+        document.querySelector("#qt").innerText = ""
+    }
+}
+
+document.querySelector("#btCar").addEventListener("click", carrinho)
+
+function carrinho (){
+    const qt = document.querySelector("#qt").innerText
+    if(qt == ""){
+        alert("Necessário selecionar 1 item.")
+        return
+    }
+    produtos.classList.toggle("ocultar")
+    divcarrinho.classList.toggle("ocultar")
+    atualizarTb()
+}
+let pedido 
+function atualizarTb(){
+    pedido =""
+    let total = 0
+    const tbody = document.querySelector("tbody")
+    tbody.innerHTML = ""
+    for (let i = 0; i< lsProdutos.length; i++){
+        const p = lsProdutos[i]
+        if(p.qt > 0){
+            tbody.innerHTML += `<tr>
+            <td>${p.nome}</td>
+            <td>${p.qt}</td>
+            <td>${p.valor.toFixed(2)}</td>
+            <td>${(p.qt * p.valor).toFixed(2)}</td>
+            <td onclick="add(${i},1)">+</td>
+            <td onclick="add(${i},-1)">-</td>
+            </tr>
+            `
+            total += p.qt * p.valor
+            pedido += `${p.nome} (${p.qt} x ${p.valor} = ${p.valor*p.qt})\n`
+        }
+    }
+    tbody.innerHTML += `
+    <tr>
+        <td colspan = "3"> Valor Final</td>
+        <td colspan = "3"> ${total.toFixed(2)}</td>
+    </tr>
+    `
+    pedido += `Total = ${total.toFixed(2)}`
+}
+
+function add(i,valor ){
+    lsProdutos[i].qt += valor
+    atualizarTb()
+    if(lsProdutos[i].qt == 0){
+        document.querySelectorAll(".produto")[i].dispatchEvent(new Event("click"))
+    }
+     const qt = lsProdutos.filter(p => p.qt >= 1)
+     if(qt == ""){
+        produtos.classList.toggle("ocultar")
+        divcarrinho.classList.toggle("ocultar")
+     }
+}
+
+const frm = document.querySelector("form")
+frm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    const nome = frm.inNome.value
+    let msg = "Desejo fazer o seguinte pedido:"
+    msg+= pedido
+    msg+=`Att: ${nome}`
+    if(confirm("Deseja enviar essa mensagem?\n"+msg)){
+        msg = encodeURI(msg)
+        const link = `https://wa.me/5561995831469?text=${msg}`
+        window.open(link, '_blank')
+    }
+})
